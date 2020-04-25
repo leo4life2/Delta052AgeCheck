e8655600f4f42381bc99108e854876ce6a71d9d9
leo4life2 <leo4life@163.com>
2020-04-26 06:31:01 +0800

    exe created

--- vaccinationAgeChecker.py
+++ vaccinationAgeChecker.py
@@ -5,69 +5,155 @@ from tkinter import scrolledtext
 import tkinter.filedialog
 import numpy as np
 
+class ShotType:
+    strict_margin = 7
+    # In Days
+    # Boundaries are *inclusive* so there are '-1's after the year #.
+    class HPV9:
+        min_age = 16 * 365.25
+
+        age_upper_bounds_lenient = [
+            (27 * 365.25 - 1) - (4 * 30),
+            (27 * 365.25 - 1) - (3 * 30),
+            (27 * 365.25 - 1)
+        ]
+
+    class HPV4:
+        min_age = 20 * 365.25
+
+        age_upper_bounds_lenient = [
+            (46 * 365.25 - 1) - (6 * 30),
+            (46 * 365.25 - 1) - (4 * 30),
+            (46 * 365.25 - 1)
+        ]
+
+    class Prev13:
+        min_age = 6 * 7
+
+        age_upper_bounds_lenient = [
+            (7 * 30),
+            (7 * 30),
+            (7 * 30),
+            (16 * 30)
+        ]
+
+        reinforcement_minage = (12 * 30)
+    
+    class Five_Vacc:
+        min_age = 2 * 30
+        
+        age_upper_bounds_lenient = [
+            (5 * 365.25 - 1),
+            (5 * 365.25 - 1),
+            (5 * 365.25 - 1),
+            (5 * 365.25 - 1)
+        ]
+    
+    class Infant_Flu:
+        min_age = 6 * 30
+        
+        age_upper_bounds_lenient = [
+            (3 * 365.25 - 1), # Upper bound is 35 months, which is equivalent to <3 years.
+            (3 * 365.25 - 1), # ! 科兴 one's upper limit is 3 Years--not sure if it means 3岁 or <3 years. !
+            (3 * 365.25 - 1),
+        ]
+        
+    class Adult_Flu:
+        min_age = 3 * 365.25
+        
+        age_upper_bounds_lenient = [
+            (99 * 365.25 - 1),
+            (99 * 365.25 - 1),
+            (99 * 365.25 - 1),
+        ]
+    
+    class Rotavirus:
+        min_age = (6 * 365)
+        
+        age_upper_bounds_lenient = [
+            (33 * 365.25 - 1),
+            (33 * 365.25 - 1),
+            (33 * 365.25 - 1),
+        ]
+    
+    class Chickenpox:
+        min_age = (1 * 365)
+        
+        age_upper_bounds_lenient = [
+            (99 * 365.25 - 1),
+            (99 * 365.25 - 1),
+            (99 * 365.25 - 1),
+        ]
+    
+    class HandFootMouth:
+        min_age = (6 * 30)
+        
+        age_upper_bounds_lenient = [
+            (6 * 365.25 - 1),
+            (6 * 365.25 - 1),
+            (6 * 365.25 - 1),
+        ]
+        
+    class JapEnceph:
+        min_age = (6 * 30)
+        
+        age_upper_bounds_lenient = [
+            (11 * 365.25 - 1),
+            (11 * 365.25 - 1),
+            (11 * 365.25 - 1),
+        ]
+    
+    class HepaA:
+        min_age = (12 * 30)
+        
+        age_upper_bounds_lenient = [
+            (18 * 365.25 - 1),
+            (18 * 365.25 - 1),
+            (18 * 365.25 - 1),
+        ]    
+    
+    name_strs = [
+        'HPV四价',
+        'HPV九价',
+        '13价',
+        '5联',
+        '小儿流感',
+        '成人流感',
+#         '五价轮状',
+        '水痘',
+        '手足口',
+        '乙脑',
+        '甲肝'
+    ]
+
+    str_to_type = {
+        'HPV四价': HPV4,
+        'HPV九价': HPV9,
+        '13价': Prev13,
+        '5联': Five_Vacc,
+        '小儿流感': Infant_Flu,
+        '成人流感': Adult_Flu,
+#         '五价轮状': Rotavirus,
+        '水痘': Chickenpox,
+        '手足口': HandFootMouth,
+        '乙脑': JapEnceph,
+        '甲肝': HepaA
+    }
+
 def get_sheet():
     filepath = tkinter.filedialog.askopenfilename()
     sheet = read_excel(filepath, na_filter = False)
     sheet_np = sheet.to_numpy()
     return filepath, sheet_np
 
-class ShotType:
-    # In Days
-    # Boundaries are *inclusive* so there are '-1's after the year #.
-    class HPV9:
-        min_age = 16 * 365.25
-
-        threeshots_age_upper_bounds_lenient = [
-            (27 * 365.25 - 1) - (4 * 30),
-            (27 * 365.25 - 1) - (3 * 30),
-            (27 * 365.25 - 1)
-        ]
-        threeshots_age_upper_bounds_strict = [bound - 7 for bound in threeshots_age_upper_bounds_lenient]
-
-    class HPV4:
-        min_age = 20 * 365.25
-
-        threeshots_age_upper_bounds_lenient = [
-            (46 * 365.25 - 1) - (6 * 30),
-            (46 * 365.25 - 1) - (4 * 30),
-            (46 * 365.25 - 1)
-        ]
-        threeshots_age_upper_bounds_strict = [bound - 7 for bound in threeshots_age_upper_bounds_lenient]
-
-    class PREV13:
-        min_age = 6 * 7
-
-        threeshots_age_upper_bounds_lenient = [
-            (7 * 30),
-            (7 * 30),
-            (7 * 30)
-        ]
-        threeshots_age_upper_bounds_strict = [bound - 7 for bound in threeshots_age_upper_bounds_lenient]
-
-        reinforcementshot_minage = (12 * 30)
-        reinforcementshot_maxage_lenient = (16 * 30)
-        reinforcementshot_maxage_strict = reinforcementshot_maxage_lenient - 7
-
 def get_parameters(shot_date_raw, bday_raw, product_type, additional_info, shot_description):
     '''
     Returns encoded shot_date, bday & shotnum
     '''
     def get_shot_type(product_description):
-        name_strs = [
-            'HPV四价',
-            'HPV九价',
-            '13价'
-        ]
-
-        str_to_type = {
-            'HPV四价': ShotType.HPV4,
-            'HPV九价': ShotType.HPV9,
-            '13价': ShotType.PREV13
-        }
-
-        for shot_str in name_strs:
+        for shot_str in ShotType.name_strs:
             if shot_str in product_description:
-                return str_to_type[shot_str]
+                return ShotType.str_to_type[shot_str]
 
     def get_shot_num(product_str, additional_info):
         if '第一针' in product_str or '第一针' in additional_info:
@@ -76,10 +162,12 @@ def get_parameters(shot_date_raw, bday_raw, product_type, additional_info, shot_
             return 2
         elif '第三针' in product_str or '第三针' in additional_info:
             return 3
-        else:
+        elif '第四针' in product_str or '第四针' in additional_info:
             # Special case like reinforcement shot (aka 4th shot for PREV13)
             return 4
-
+        else:
+            return -1
+        
     shot_date = datetime.strptime(shot_date_raw, '%Y-%m-%d')
     bday = datetime.strptime(bday_raw, '%Y/%m/%d')
     shot_num = get_shot_num(product_type, additional_info)
@@ -88,7 +176,6 @@ def get_parameters(shot_date_raw, bday_raw, product_type, additional_info, shot_
     return shot_date, bday, shot_num, shot_type
 
 def check_shot_age(shottype, shotnum, shot_date, bday):
-
     def datediff(d1, d2):
         return (d2 - d1).days
 
@@ -103,17 +190,16 @@ def check_shot_age(shottype, shotnum, shot_date, bday):
 
     # lowerlimit is SAME for both Strict & Lenient standards.
     lowerlimit = -1
-
-    if shottype is ShotType.HPV9 or shottype is ShotType.HPV4 or shottype is ShotType.PREV13 and shotnum in range(1,4):
-        # regular 3-shot vaccine
-        upperlimit_strict = shottype.threeshots_age_upper_bounds_strict[shotnum-1]
-        upperlimit_lenient = shottype.threeshots_age_upper_bounds_lenient[shotnum-1]
-        lowerlimit = shottype.min_age
-    elif shottype is ShotType.PREV13 and shotnum not in range(3):
-        # reinforcement shots for PREV13
-        upperlimit_strict = shottype.reinforcementshot_maxage_strict
-        upperlimit_lenient = shottype.reinforcementshot_maxage_lenient
-        lowerlimit = shottype.reinforcementshot_minage
+    
+    # Shot type is never None.
+    # Kinda gambling here, betting that noone's gonna make a mistake and write 4th shot for HPVs.
+    upperlimit_lenient = shottype.age_upper_bounds_lenient[shotnum-1]
+    upperlimit_strict = upperlimit_lenient - ShotType.strict_margin
+    lowerlimit = shottype.min_age
+    
+    # Prev13 special case. Has different min age for 4th shot.
+    if shotnum == 4 and shottype is ShotType.Prev13:
+        lowerlimit = shottype.reinforcement_minage
 
     # Check lower boundaries first
     if not does_satisfy_age_limit(shot_date, bday, lowerlimit, check_lowerbound = True):
@@ -121,9 +207,9 @@ def check_shot_age(shottype, shotnum, shot_date, bday):
 
     # Check strict & lenient upper bounds
     if not does_satisfy_age_limit(shot_date, bday, upperlimit_strict):
-        return '！警告：年龄过大。年龄（天）：' + str(datediff(bday, shot_date)) +'，上限：' + str(upperlimit_strict)
+        return '！警告：年龄过大。年龄（天）：' + str(datediff(bday, shot_date)) +'，上限：' + str(upperlimit_strict) + '\n'
     if not does_satisfy_age_limit(shot_date, bday, upperlimit_lenient):
-        return '提醒：即将超龄。年龄（天）：' + str(datediff(bday, shot_date)) +'，上限：' + str(upperlimit_strict) # Using strict bound for display
+        return '提醒：即将超龄。年龄（天）：' + str(datediff(bday, shot_date)) +'，上限：' + str(upperlimit_strict) + '\n' # Using strict bound for display
 
     return '通过'
 
@@ -134,16 +220,18 @@ def check_sheet_vacc_ages(sheet):
     errors = ''
 
     for row in sheet[3:]: # First 3 rows excluded (备注 counted as label for pandas)
-        # Col index 1 is shot date
-        # Col index 7 is patient bday
+        # *Col index 1 is shot date
+        # *Col index 7 is patient bday
         # Col index 13 is status. Exclude 取消 status data.
         # Col index 16 is additional info (look here if shot # not indicated in col ix 17)
         # Col index 17 is 就诊类型（疫苗 -- vaccine shot num appears here
         # Col index 18 is 就诊原因（套餐/券 -- vaccine type appears here
+        # * = Required. Skip if missing.
+        
         status = row[13]
         product = row[18]
 
-        if product is not np.nan and status != '取消':
+        if product is not np.nan and status != '取消' and row[1] != '' and row[7] != '':
 
             shot_date_raw = row[1][:10] # Only need date; exclude hours and mins
             bday_raw = row[7]
@@ -152,14 +240,14 @@ def check_sheet_vacc_ages(sheet):
 
             shot_date, bday, shot_num, shot_type = get_parameters(shot_date_raw, bday_raw, product_type, additional_info, product)
 
-            if shot_type is None and product != '':
+            if shot_type is None:
                 unknown_types.append(product)
                 continue
 
             name = row[3]
 
             row_str = '姓名：' + name + '，预约时间：' + shot_date_raw + '，类型：' + product + '，第' + str(shot_num) + '针 ' + '，出生日期：' + bday_raw
-
+            
             check_result = check_shot_age(shot_type, shot_num, shot_date, bday)
             if check_result != '通过':
                 errors += (row_str + '\n' + check_result + '\n')
@@ -180,7 +268,7 @@ def check_sheet_vacc_ages(sheet):
 
 window = Tk()
 window.title("自动检查疫苗年龄要求程序:D")
-window.geometry('400x500')
+window.geometry('800x500')
 
 path, np_sheet = None, None
 def choose_file():
@@ -195,9 +283,8 @@ def analyze():
         return
     results = check_sheet_vacc_ages(np_sheet)
     txt.insert(1.0, results)
-    txt['state'] = 'disabled'
-
-
+#     txt['state'] = 'disabled'
+    
 lbl = Label(window, text="请先选择一个文件", anchor = 'center', wraplength = 300, font = ('Songti SC', 15))
 lbl.grid(column = 0, row = 0)
 
@@ -210,7 +297,7 @@ analyzebtn.grid(column = 0, row = 2)
 scroll = Scrollbar(window)
 scroll.grid(column = 1, row = 3, sticky = 'ns')
 
-txt = Text(window, height=15, width=45)
+txt = Text(window, height=15, width=95)
 txt.grid(column = 0, row = 3)
 
 scroll.config(command=txt.yview)
---
+++ vaccinationAgeChecker.spec
@@ -0,0 +1,37 @@
+# -*- mode: python ; coding: utf-8 -*-
+
+block_cipher = None
+
+
+a = Analysis(['vaccinationAgeChecker.py'],
+             pathex=['C:\\Users\\leo4l\\Desktop\\Delta052AgeCheck'],
+             binaries=[],
+             datas=[],
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher,
+             noarchive=False)
+pyz = PYZ(a.pure, a.zipped_data,
+             cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          [],
+          exclude_binaries=True,
+          name='vaccinationAgeChecker',
+          debug=False,
+          bootloader_ignore_signals=False,
+          strip=False,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               upx_exclude=[],
+               name='vaccinationAgeChecker')
